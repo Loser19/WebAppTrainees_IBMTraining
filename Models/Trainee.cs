b9c86c02f40f7d741d0a8f36e0cabbe849a1f6fa
Sounak <sounak.avash@gmail.com>
2025-08-25 10:28:11 +0530

    Add project files.

---
+++ Models/Trainee.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations; // Needed for data annotations
+
+namespace WebAppTrainees.Models
+{
+    public class Trainee
+    {
+        [Key] // This attribute indicates that this property is the primary key
+        public int Id { get; set; } 
+
+        [Required] // This attribute indicates that this property is required and is not null
+        public string Name { get; set; } = string.Empty;
+
+        [EmailAddress] // This attribute validates that the string is a valid email address
+        public string Email { get; set; } = string.Empty;
+
+        // not adding anything means it can take null
+        public string Address { get; set; } = string.Empty;
+        public Trainee() { }
+        public Trainee(int id, string name, string email, string address)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+            Address = address;
+        }
+        public override string ToString()
+        {
+            return $"Id: {Id}, Name: {Name}, Email: {Email}, Address: {Address}";
+        }
+    }
+}
+
---
+++ Models/TraineeContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore; // Needed for DbContext
+namespace WebAppTrainees.Models
+{
+    public class TraineeContext: DbContext // Inherits from DbContext to interact with the database
+    {
+        public TraineeContext() { }
+        public TraineeContext(DbContextOptions<TraineeContext> options) : base(options) { }
+        //static string connectionString = @"Data Source=IBM-DJC3SB4\SQLEXPRESS;Initial Catalog=TraineeDB;TrustServerCertificate=True;Integrated Security=True;"; // Static field to hold the connection string
+        public DbSet<Trainee> Trainees { get; set; } = null!; // DbSet for Trainee entities
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured) // Check if options are already configured
+            {
+                base.OnConfiguring(optionsBuilder); // Call the base method to ensure proper configuration
+                //optionsBuilder.UseSqlServer(connectionString); // Use SQL Server with the connection string
+            }
+        }
+        /*protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Trainee>().ToTable("Trainees"); // Map the Trainee entity to the "Trainees" table
+            base.OnModelCreating(modelBuilder); // Call the base method to apply configurations
+        }*/    
+
+    }
+}
